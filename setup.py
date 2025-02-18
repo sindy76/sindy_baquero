203c41a2572113bb62db3e9d99a12f1dd9336901
Sindy baquero <sindy.baquero@est.iudigital.edu.co>
2025-02-18 15:32:19 -0500

    actividad_1 - configuracion

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+
+from setuptools import setup,find_packages
+
+setup (
+    name= "sindy_baquero",
+    version= "0.0.1",
+    author= "Sindy Baquero",
+    author_email= "sindy.baquero@est.iudigital.edu.co",
+    description= "",
+    py_modules= ["actividad_1"],
+    install_requires= [
+        "pandas",
+        "matplotlib"
+ ],
+ )
