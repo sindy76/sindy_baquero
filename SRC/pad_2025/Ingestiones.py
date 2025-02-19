8c4c41ec854e89d713b32bd9419da7397fb5c810
Sindy baquero <sindy.baquero@est.iudigital.edu.co>
2025-02-19 16:03:38 -0500

    actualizar 1

--- SRC/pad_2025/Ingestiones.py
+++ SRC/pad_2025/Ingestiones.py
@@ -6,7 +6,7 @@ import json
 
 class Ingestiones():
     def __init__(self):
-        self.ruta_static = "G:/P_Paola/IUD/Analitica de dato/Reposotorio/sindy_baquero/SRC/pad_2025/static/json"
+        self.ruta_static = "G:/P_Paola/IUD/Analitica de dato/Reposotorio/sindy_baquero/SRC/pad_2025/Static"
 def leer_json(self):
         # r read w write
 
