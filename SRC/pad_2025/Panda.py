2a40d2e716e7d20ce0147a2f262b9adaf672f174
Sindy baquero <sindy.baquero@est.iudigital.edu.co>
2025-03-04 21:44:29 -0500

    Configuracion panda

--- SRC/pad_2025/Panda.py
+++ SRC/pad_2025/Panda.py
@@ -2,23 +2,31 @@ import numpy as np
 import pandas as pd
 from numpy.random import randn
 
-
 class Matrices():
-    def __init__(self, data):
-          np.random.seed(101)
+    def __init__(self):
+        np.random.seed(101)
     
-    def serie(self):
-        etiquetas = ['A', 'B', 'C', 'D', 'E']
-        valores = [10, 20, 30, 40, 50]
+    def serie(self): # una sola dimencion
+        etiquetas= ["a","b","c"]
+        valores = [10,20,30]
         arr = np.array(valores)
-        
-        #directorios= ["amiboseries":"Mario sport superstares","character":"Metal Mario","numero"]
-        #mi_serie = pd.Series(data-valores)
+        directorios={"amiiboSeries": "Mario Sports Superstars", "character": "Metal Mario","numero":121}
+        #print(pd.__version__)
+        mi_serie= pd.Series(data=valores,index=etiquetas)
         print(etiquetas)
-        print(arr)
         print(valores)
-    def matrices (self, filas=0, columnas=0):
-        data-pd.
+        print(arr)
+        print(directorios)
+    
+    def matrices(self,filas=0,columnas=0):
+        #data= pd.DataFrame() # vacio
+        #data= pd.DataFrame(randn(6,4),index="A B C D E F".split(" "), columns="W X Y Z".split(" "))
+        data= pd.DataFrame(randn(6,4), columns="W X Y Z".split(" ")) # ((w*y)-z)+y  
+        print(data)
+        data["udigital"]= "pad"
+        data["funcion"]= ((data["W"]*data["Y"])-data["Z"])+data["Y"]
+        data.to_excel("datos_generados.xlsx")
 
-        matr = Matrices()
-       # matr.serie()
+matr =  Matrices()
+matr.matrices()
+#matr.serie()
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup (
     install_requires= [
         "pandas",
         "matplotlib",
+        "openpyxl",
         "requests"
  ]
  )
