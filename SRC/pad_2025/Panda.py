27daf937a40176050cd7c6fe4528e7f0a8a62c5c
Sindy baquero <sindy.baquero@est.iudigital.edu.co>
2025-03-04 20:41:38 -0500

    configuracion panda

---
+++ SRC/pad_2025/Panda.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from numpy.random import randn
+
+
+class Matrices():
+    def __init__(self, data):
+          np.random.seed(101)
+    
+    def serie(self):
+        etiquetas = ['A', 'B', 'C', 'D', 'E']
+        valores = [10, 20, 30, 40, 50]
+        arr = np.array(valores)
+        
+        #directorios= ["amiboseries":"Mario sport superstares","character":"Metal Mario","numero"]
+        #mi_serie = pd.Series(data-valores)
+        print(etiquetas)
+        print(arr)
+        print(valores)
+    def matrices (self, filas=0, columnas=0):
+        data-pd.
+
+        matr = Matrices()
+       # matr.serie()
