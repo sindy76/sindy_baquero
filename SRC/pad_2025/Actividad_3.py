0e924be5d1e6c12607a986306f49160700165a00
sindy <sindy.baquero@est.iudigital.edu.co>
2025-03-21 11:24:45 -0500

    Ejecicios actividad_3

--- SRC/pad_2025/Actividad_3.py
+++ SRC/pad_2025/Actividad_3.py
@@ -10,21 +10,44 @@ class ejercicios:
         self.ruta_raiz=os.path.abspath(os.getcwd())
         self.ruta_Actividad_3 = "{}/SRC/pad_2025/Actividad_3/".format(self.ruta_raiz)
 
-
     def ejercicio1(self):
 
         # Crea un DataFrame frutas
         frutas = pd.DataFrame({
-            "":[0],
             "Granadilla": [20],
             "Tomates": [50]})
         ruta_ej1 = os.path.join(self.ruta_Actividad_3, "Ejercicio_1.csv")
         frutas.to_csv(ruta_ej1, index=False)
+        print(frutas)
+
+    def ejercicio2(self):
+        # Crea un DataFrame ventas_frutas que coincida con el diagrama:
+        ventas_frutas = pd.DataFrame(
+    {"Granadilla": [20, 49], "Tomates": [50, 100]},
+    index=["ventas 2021", "ventas 2022"])
+        ruta_ej2 = os.path.join(self.ruta_Actividad_3, "Ejercicio_2.csv")
+        ventas_frutas.to_csv(ruta_ej2, index=True)
+        print(ventas_frutas)
+
+
+    def ejercicio3(self):
+        #Crea una variable utensilios con una Serie que tenga el siguiente aspecto:
+        utensilios = pd.Series(
+    data=["3 unidades", "2 unidades", "4 unidades", "5 unidades"],
+    index=["Cuchara", "Tenedor", "Cuchillo", "Plato"],
+    name="cocina")
+        ruta_ej3 = os.path.join(self.ruta_Actividad_3, "Ejercicio_3.csv")
+        utensilios.to_csv(ruta_ej3, index=True)
+        print(utensilios)
+       
+
+                          
+
 
     def ejecutar(self):
         self.ejercicio1()
-        #self.ejercicio2()
-        #self.ejercicio3()
+        self.ejercicio2()
+        self.ejercicio3()
       
 
         
